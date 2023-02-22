a46d4be02712f8a5f09af02dcf4f5c572fa8ce29
SalimcanK <salimcankaya97@gmail.com>
2023-02-22 17:04:05 +0300

    repo implementation

--- LoanApplication/src/main/java/com/salimcankaya/repository/CustomerRepository.java
+++ LoanApplication/src/main/java/com/salimcankaya/repository/CustomerRepository.java
@@ -1,16 +1,22 @@
 package com.salimcankaya.repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.salimcankaya.model.Customer;
 
+import jakarta.transaction.Transactional;
 
+
+@Transactional
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	
 	Optional<Customer> findByTckn(Long tckn);
 	
 	boolean deleteByTckn(Long tckn);
+	
+	boolean existsByDateOfBirth(LocalDate dateOfBirth);
 }
--- LoanApplication/src/main/java/com/salimcankaya/repository/LoanRepository.java
+++ LoanApplication/src/main/java/com/salimcankaya/repository/LoanRepository.java
@@ -1,10 +1,20 @@
 package com.salimcankaya.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.salimcankaya.model.Loan;
 
+import jakarta.transaction.Transactional;
 
+
+@Transactional
 public interface LoanRepository extends JpaRepository<Loan, Long> {
+	
+	
+	List<Loan> findLoansByCustomer_Tckn(Long tckn);
+	
+	boolean deleteLoansByCustomer_Tckn(Long tckn);
 
 }
